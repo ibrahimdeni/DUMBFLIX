384e1f1bc7ccf4babee72ca2e2f2db8733725574
unknown <ibrahimdeniharyanto@gmail.com>
2022-09-05 10:02:26 +0700

    nambah movies adn tvshows

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import Layout from './widgets/Layout';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-// import Home from './pages/Home';
-// import Movies from './pages/Movies';
-// import TvShows from './pages/TvShows';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import TvShows from './pages/TvShows';
 // import Profile from './pages/Profile';
 // import Payment from './pages/Payment';
 // import VideoDetail from './pages/VideoDetail';
@@ -12,24 +12,27 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <>
       <Route />
         <Route path='/' element={
           <Layout>
-            {/* <Home /> */}
+            <Home />
           </Layout>
         } />
-{/*   
-        <Route path='/tvshows' element={
-          <Layout>
-            <TvShows />
-          </Layout>
-        }/>
-  
+
         <Route path='/movies' element={
           <Layout>
             <Movies />
           </Layout>
         }/>
+
+        <Route path='/tvshows' element={
+          <Layout>
+            <TvShows />
+          </Layout>
+        }/>
+{/*   
+    
   
         <Route path='/video' element={
           <Layout>
@@ -48,6 +51,7 @@ function App() {
             <Payment />
           </Layout>
         }/> */}
+        </>
     </Routes>
     </BrowserRouter>
   );
